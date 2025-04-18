af99c03467588b554ad093b5d79652d428b3654c
Belfor Acuña <131080733+belfor-acuna@users.noreply.github.com>
2025-04-18 19:53:49 -0400

    build(CI/CD): :closed_lock_with_key: Change CORS into a parameter
    Added CORS via env variable

--- src/main/java/com/ufro/dci/etransparency/etransparency_api_user/config/security/SecurityConfig.java
+++ src/main/java/com/ufro/dci/etransparency/etransparency_api_user/config/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.ufro.dci.etransparency.etransparency_api_user.config.security;
 
 import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -36,6 +38,10 @@ public class SecurityConfig {
     private final JwtUtils jwtUtils;
     private final JwtAuthorizationFilter authorizationFilter;
     private final AccessDeniedHandler customAccessDeniedHandler;
+
+    @Value("${origin.url}")
+    private String originUrl;
+
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -95,7 +101,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource websiteConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://www." + originUrl,"https://" + originUrl));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
         configuration.setAllowCredentials(true);
